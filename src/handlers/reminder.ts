82dadd8fe5880717125c6028e5deba34a5987498
Moldoteck <cpadureac@gmail.com>
2022-08-24 23:55:36 +0300

    list reminders

--- src/handlers/reminder.ts
+++ src/handlers/reminder.ts
@@ -193,7 +193,27 @@ export async function handleList(ctx: Context) {
     //remaining to date
     let date = new Date(timeoutNumber)
 
-    let finalMessage = `Reminder for ${username}\nTriggered on:${date.toDateString()}:${msg}${repl}`
+    // get total seconds between the times
+    var delta = Math.abs(timeoutNumber - Date.now()) / 1000
+
+    // calculate (and subtract) whole days
+    var days = Math.floor(delta / 86400)
+    delta -= days * 86400
+
+    // calculate (and subtract) whole hours
+    var hours = Math.floor(delta / 3600) % 24
+    delta -= hours * 3600
+
+    // calculate (and subtract) whole minutes
+    var minutes = Math.floor(delta / 60) % 60
+    delta -= minutes * 60
+
+    // what's left is seconds
+    var seconds = delta % 60
+
+    let timeRemaining = `${days}d ${hours}h ${minutes}m ${seconds}s`
+
+    let finalMessage = `Reminder for ${username}\nWill trigger on:${date.toDateString()}\n after ${timeRemaining}:${msg}${repl}`
     if (
       ctx?.dbchat?.id == ctx.dbuser.id ||
       ctx.dbchat?.id?.toString() == reminder.chatID
